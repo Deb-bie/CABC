31727714d9a0fa0e58f51b401b27bae855c0386a
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-04-11 21:21:25 -0500

    batch size and epochs reduced

--- breast_cancer_1.py
+++ breast_cancer_1.py
@@ -18,8 +18,8 @@ import tensorflow_addons as tfa # type: ignore
 data_path = "../../../data/BreaKHis_Total_dataset"
 labels = ['benign', 'malignant']
 img_size = 224
-batch_size = 48
-epochs = 30
+batch_size = 10
+epochs = 10
 
 def loading_data(data_dir):
     data = []
